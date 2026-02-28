9901ab89c32930d29f257e533890a841b3e687dc
ssheng2000 <shenglun2000@gmail.com>
2026-02-28 14:42:44 +0000

    Pause button working

--- Scripts/pause_button.gd
+++ Scripts/pause_button.gd
@@ -8,16 +8,20 @@ func _ready():
 	GlobalEventBus.build_mode_changed.connect(_on_build_mode_changed)
 	pressed.connect(_on_pressed)
 	_refresh_text()
+	print("button init")
 
 func set_pauses_remaining(v: int):
 	pauses_remaining = v
+	print("button", v)
 	_refresh_text()
 
 func _on_build_mode_changed(enabled: bool):
 	build_mode = enabled
+	print("build mode occurs")
 	_refresh_text()
 
 func _on_pressed():
+	print("clicked")
 	if build_mode:
 		GlobalEventBus.unpause.emit()
 	else:
@@ -30,4 +34,5 @@ func _refresh_text():
 		disabled = false
 	else:
 		text = "Build (%d)" % pauses_remaining
+		print(text)
 		disabled = (pauses_remaining <= 0)
